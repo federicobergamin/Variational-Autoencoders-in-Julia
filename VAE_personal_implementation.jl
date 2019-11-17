63f70484715d0bf9f28393e2f57a19b65661c701
Federico Bergamin <bergamin.federico@gmail.com>
2019-11-17 18:05:59 +0100

    First commit VAE in Julia

---
+++ VAE_personal_implementation.jl
@@ -0,0 +1,177 @@
+## I try to implement again a simple VAE using Julia
+## There is a tutorial with a working VAE in the Flux documentation
+## (https://github.com/jessebett/model-zoo/blob/master/vision/mnist/vae.jl)
+## I try to do mine in a different way
+
+using Flux, Statistics
+using Flux: Tracker, throttle, params, binarycrossentropy, gradient, @epochs
+#flux function for update parameters
+using Flux.Tracker: update!
+using Juno: @progress
+using MLDataUtils
+using Images
+include("utils.jl")
+
+img(x) = Gray.(permutedims(reshape(x, 28, 28)))
+
+function log_standard_gaussian(x)
+    #return torch.sum(-0.5 * math.log(2 * math.pi) - x ** 2 / 2, dim=-1)
+    #log_pdf =  -0.5f0 * log(2 * pi) .- x.^2 / 2
+    log_pdf =  @. -0.5f0 * log(2 * pi) - x^2 / 2
+    #@show(A)
+    # alternatively, we can do
+    #Norm = Normal(0)
+    #A = log(pdf(Norm, x))
+    #@show(A)
+    return log_pdf
+end
+
+# function log_gaussian(x, mu, log_var)
+#     std = exp.(log_var * 0.5f0)
+#     #@show log_var
+#     #@show std
+#     Norm = Normal(mu, std)
+#     A = log(pdf(Norm, x))
+#     return A
+# end
+
+function log_gaussian(x, mu, log_var)
+    log_pdf = - 0.5 * log(2 * pi) .- log_var / 2 - (x .- mu)^2 / (2 * exp.(log_var))
+    #log_pdf = @. - 0.5 * log(2 * pi) - (log_var + eps(Float32))1 / 2 - (x - mu)^2 / (2 * exp(log_var))
+    # print('Size log_pdf:', log_pdf.shape)
+    return log_pdf
+end
+
+
+# function binary_cross_entropy(recon, x)
+#     # we can also use "using Flux: binarycrossentropy"
+#     # sum(binarycrossentropy.(recon, x))
+#     s = @. x * log(recon + Float32(1e-8)) + (1 - x) * log(1 - recon + Float32(1e-8))
+#     return -sum(s)
+# end  THIS FUNCTION IS SUPER SLOW, MY GOODNESS! USE THE ONE PROVIDE BY FLUX
+
+function reparametrization_trick(mu, log_var)
+    z = mu + rand(Float32) * exp(log_var * 0.5)
+    return z
+end
+
+function analytic_kl(mu, log_var)
+    kl = (log_var - mu.^2 .+ 1.0f0 - exp.(log_var)) * 0.5
+    return kl
+end
+
+function _kl_divergence(z, mu_q, log_var_q, mu_p = nothing, log_var_p = nothing)
+    # instead of computing it analytically, we can estimate it
+    # using monte carlo methods
+    qz = log_gaussian.(z, mu_q, log_var_q)
+    ## we should do the same with p
+    if isnothing(mu_p) & isnothing(log_var_p)
+        pz = log_standard_gaussian.(z)
+    else
+        pz = log_gaussian.(z, mu_p, log_var_p)
+    end
+    kl = qz - pz
+    return kl
+end
+
+const batch_size = 64
+const latent_dim = 2
+const hidden_dim = 400
+const n_epochs = 20
+
+# we can load the MNIST dataset
+x_train, x_valid, x_test = load_binarized_mnist(batch_size, true)
+example = x_train[:,1]
+save("original2.png", img(example))
+
+N = size(x_train, 2)
+x_train_batch = [x_train[:,i] for i in Iterators.partition(1:N,batch_size)]
+
+# we have to create the encoder
+h, mu, log_var = Dense(28*28, hidden_dim, relu), Dense(hidden_dim, latent_dim), Dense(hidden_dim, latent_dim)
+encoder(X) = (hidden_activation = h(X); (mu(hidden_activation), log_var(hidden_activation)))
+
+# the decoder
+decoder = Chain(Dense(latent_dim, hidden_dim, relu),
+                Dense(hidden_dim, 28*28, sigmoid))
+
+# we define the
+# we have to define a method to samples
+get_sample() = decoder(randn(Float32,latent_dim))
+
+# now we have to define the callback --> used to observe the training process
+function compute_loss(_data::Matrix)
+    # we should pass our data into the encoder
+    (_mu, _log_var) = encoder(_data)
+    #@show size(_mu)
+    #@show size(_log_var)
+    # then we use the reparametrization trick to sample z
+    _z = reparametrization_trick.(_mu, _log_var)
+    # we pass the z through the decoder to get the mean for each pixel
+    mu_pixels = decoder(_z)
+
+    # now we should compute the compute_loss
+    likelihood = - sum(binarycrossentropy.(mu_pixels, _data))
+    #@show likelihood
+    kl = - analytic_kl(_mu, _log_var)
+    #kl = _kl_divergence(_z, _mu, _log_var)
+    #©@show sum(kl)
+    elbo = likelihood - sum(kl)
+    loss = -elbo
+    #@show loss
+
+    #@show loss
+
+    return loss, -likelihood, kl
+end
+
+opt = ADAM(1e-4)
+ps = params(h, mu, log_var, decoder)
+
+#
+function train!()
+    for ep in 1:n_epochs
+        @info "Training Epoch $ep..."
+        tot_recon = 0
+        tot_kl = 0
+        tot_loss = 0
+        j = 0
+        for data in x_train_batch
+            #@show j
+            j += 1
+            #we have to compute the loss of this
+            (loss, _recon_err, _kl) = compute_loss(data)
+            grad = Flux.Tracker.gradient(()->loss/size(data,2), ps)
+            update!(opt, ps, grad)
+            tot_recon += _recon_err
+            tot_kl += sum(_kl)
+            tot_loss += loss
+            #Flux.back!(l)<
+            #Flux.Optimise._update_params!(opt, ps)
+        end
+            # we are at the end of the epochs
+        println("Epoch: $ep, -ELBO: $(tot_loss/size(x_train,2)), recon err: $(tot_recon/size(x_train,2)), KL: $(tot_kl/size(x_train,2))")
+        ## we can also save some samples ans see some reconstructions
+        ## PROBLEMS HERE
+        sample = hcat(img.([get_sample() for i = 1:32])...)
+        save("samples_from_random_epoch_$ep.png", sample)
+
+        example = x_train[:,123]
+        save("original_epoch_$ep.png", img(example))
+        m, lv = encoder(example)
+        zed2 = reparametrization_trick.(m,lv)
+        recon = decoder(zed2)
+
+        recon_to_save = img(recon)
+        save("reconstruction_epoch_$ep.png", recon_to_save)
+    end
+end
+
+train!()
+#
+#@epochs n_epochs train!()
+# evalcb = throttle(() -> @show(ELBO(x_train[:, rand(1:N, batch_size)])/batch_size) , 30)
+# @progress for i = 1:10
+#   @info "Epoch $i"
+#   Flux.train!(loss, ps, zip(x_train_batch), opt, cb=evalcb)
+# end
---
+++ utils.jl
@@ -0,0 +1,135 @@
+## we implement a method to load the original MAT file of then
+## binarized MNIST
+
+using MAT
+using MLDataUtils
+using Distributions
+import Distributions: logpdf
+using MLDatasets: MNIST
+
+
+img(x) = Gray.(permutedims(reshape(x, 28, 28)))
+
+
+function load_binarized_mnist(batch_size, flatten)
+    # we start by loading the training set
+    file = open("Original_MNIST_binarized/binarized_mnist_train.amat", "r")
+    train_imgs = readlines(file)
+    #C =  findall(x->x=="1", vec(train_imgs[2,:]))
+    #@show C
+    train_imgs = [parse.(Float32, split(train_imgs[i]," ")) for i in 1:size(train_imgs, 1)]
+    example = train_imgs[1]
+    save("check_while_load_fin.png", img(example))
+    #C =  findall(x->x==1.0, vec(train_imgs[2,:][1]))
+    #@show C
+    #train_imgs = permutedims(reshape(hcat(train_imgs...), (length(train_imgs[1]), length(train_imgs))))
+    train_imgs = hcat(train_imgs...)
+    @show(size(train_imgs))
+    example = train_imgs[:,1]
+    save("check_while_load_fin2.png", img(example))
+    #C =  findall(x->x==1.0, vec(train_imgs[2,:]))
+    #@show C
+    @show(size(train_imgs))
+
+    # valid test
+    file = open("Original_MNIST_binarized/binarized_mnist_valid.amat", "r")
+    valid_imgs = readlines(file)
+    valid_imgs = [parse.(Float32, split(valid_imgs[i]," ")) for i in 1:size(valid_imgs, 1)]
+    valid_imgs = hcat(valid_imgs...)
+    #valid_imgs = permutedims(reshape(hcat(valid_imgs...), (length(valid_imgs[1]), length(valid_imgs))))
+    #valid_imgs = permutedims(reshape(hcat(valid_imgs...), (length(valid_imgs), length(valid_imgs[1]))))
+    #@show(size(valid_imgs))
+
+    # test set
+    file = open("Original_MNIST_binarized/binarized_mnist_test.amat", "r")
+    test_imgs = readlines(file)
+    test_imgs = [parse.(Float32, split(test_imgs[i]," ")) for i in 1:size(test_imgs, 1)]
+    test_imgs = hcat(test_imgs...)
+    #test_imgs = permutedims(reshape(hcat(test_imgs...), (length(test_imgs[1]), length(test_imgs))))
+    #test_imgs = permutedims(reshape(hcat(test_imgs...), (length(test_imgs), length(test_imgs[1]))))
+
+    #@show(size(test_imgs))
+
+    # flatten
+    if flatten
+        # we want to reshape them into (L, BS)
+        x_train = train_imgs
+        #@show x_train[:,2]
+        #C =  findall(x->x==1.0, vec(train_imgs[:,2]))
+        #@show C
+        #@show(size(x_train))
+        #x_train_batch = batchview(x_train, batch_size)
+        #@show(size(x_train_batch))
+
+        x_valid = valid_imgs
+        #@show(size(x_valid))
+        #x_valid = batchview(x_valid, batch_size)
+        #@show(size(x_valid))
+
+        x_test = test_imgs
+        #@show(size(x_test))
+        #x_test = batchview(x_test, batch_size)
+        #@show(size(x_test))
+
+    else
+        # x_train = transpose(train_imgs)
+        x_train = reshape(train_imgs, 28, 28, 1, size(train_imgs, 2))
+        #C = findall(x->x==1.0, vec(x_train[:,:,:,2]))
+        #@show C
+        #@show x_train[:,:,:,2]
+        #x_train_batch = batchview(x_train, batch_size)
+        #@show(size(x_train_batch))
+
+        # x_valid = transpose(valid_imgs)
+        x_valid = reshape(valid_imgs, 28, 28, 1, size(valid_imgs, 2))
+        #@show(size(x_valid))
+        #x_valid = batchview(x_valid, batch_size)
+        #@show(size(x_valid))
+
+        # x_test = transpose(test_imgs)
+        x_test = reshape(test_imgs, 28, 28, 1, size(test_imgs, 2))
+        #@show(size(x_test))
+        #x_test = batchview(x_test, batch_size)
+        #@show(size(x_test))
+
+    end
+
+    return x_train, x_valid, x_test
+end
+
+
+#const batch_size = 64
+#x_train, x_valid, x_test = load_binarized_mnist(batch_size, false)
+
+## function to load MNIST (credit: Jesse Bettencourt)
+function loadMNIST(batch_size)
+    # we use MLDataUtils LabelEnc for the one-hot-conversion (in the VAE we do not care about labels)
+    onehot(labels_raw) =  convertlabel(LabelEnc.OneOfK, labels_raw, LabelEnc.NativeLabels(collect(0:9)))
+    # load MNIST
+    imgs, labels_raw = MNIST.traindata();
+    # process the images into (H,W,C,BS) batches
+    @show(size(imgs))
+    x_train = reshape(imgs, size(imgs, 1), size(imgs, 2), 1, size(imgs, 3))
+    @show(size(x_train))
+    x_train = batchview(x_train, batch_size)
+    @show(size(x_train))
+    # one-hot and batch the labels
+    y_train = onehot(labels_raw)
+    y_train = batchview(y_train, batch_size)
+
+    ## we should do the same with the test set
+    imgs, labels_raw = MNIST.testdata();
+    # process the images into (H,W,C,BS) batches
+    x_test = reshape(imgs, size(imgs, 1), size(imgs, 2), 1, size(imgs, 3))
+    x_test = batchview(x_test, batch_size)
+    # one-hot and batch the labels
+    y_test = onehot(labels_raw)
+    y_test = batchview(y_test, batch_size)
+
+    return x_train, y_train, x_test, y_test
+end
+
+#const batch_size = 64
+#x_train, x_valid, x_test = load_binarized_mnist(batch_size, true)
+#@show size(x_train)
+#x_train, y_train, x_test, y_test = loadMNIST(batch_size)
